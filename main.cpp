8d342a58263471058e94a19ac088245056f09960
valecastilla <a01028209@tec.mx>
2025-09-02 16:24:20 -0600

    Added reading file and sort function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "nodo.h"
 #include "grafo.h"
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char const *argv[]) {
 
@@ -8,24 +13,24 @@ int main(int argc, char const *argv[]) {
     Nodo * n2 = new Nodo(2);
     Nodo * n3 = new Nodo(3);
 
-    vector<vector<int>> listaPesosOrdenada;
-    listaPesosOrdenada = {
-        {6, 7, 1}, 
-        {2, 8, 2}, 
-        {5, 6, 2},
-        {2, 5, 4},
-        {0, 1, 4},
-        {6, 8, 6},
-        {2, 3, 7},
-        {7, 8, 7},
-        {0, 7, 8},
-        {1, 2, 8},
-        {3, 4, 9},
-        {4, 5, 10},
-        {1, 7, 11},
-        {3, 5, 14},
-    
-    };
+    vector<vector<int> > listaPesosOrdenada;
+     
+    // Leer desde el archivo lista.txt
+    ifstream infile("lista.txt");
+    string line;
+    while (getline(infile, line)) {
+        istringstream iss(line);
+        vector<int> elemento(3);
+        iss >> elemento[0] >> elemento[1] >> elemento[2];
+        listaPesosOrdenada.push_back(elemento);
+    }
+
+    // Ordenar por el último elemento de cada subvector
+    sort(listaPesosOrdenada.begin(), listaPesosOrdenada.end(),
+        [](const vector<int>& a, const vector<int>& b) {
+            return a[2] < b[2];
+        }
+    );
     
 
     
