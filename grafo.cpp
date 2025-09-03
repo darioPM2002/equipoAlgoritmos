01833f3b93d756a08549995094c2ae69c9dab2aa
Fausto Izquierdo <fizquiv@gmail.com>
2025-09-02 22:56:03 -0600

    fix minor issues and edge case handling

--- grafo.cpp
+++ grafo.cpp
@@ -3,15 +3,17 @@
 #include <string>
 #include <vector>
 
-
 Grafo::Grafo()
 {
     indexRespuesta = {};
 }
 
-bool existeNodo(const std::vector<Nodo*>& nodos, int idBuscado) {
-    for (const auto& nodo : nodos) {
-        if (nodo->getId() == idBuscado) {
+bool existeNodo(const std::vector<Nodo *> &nodos, int idBuscado)
+{
+    for (const auto &nodo : nodos)
+    {
+        if (nodo->getId() == idBuscado)
+        {
             return true;
         }
     }
@@ -24,56 +26,49 @@ void Grafo::listaANodos(vector<vector<int>> l)
     {
         if (existeNodo(nodos, l[i][0]))
         {
-          
-        }else{
+        }
+        else
+        {
             cout << "Creando nodo " << l[i][0] << endl;
-          
+
             nodos.push_back(new Nodo(l[i][0]));
         }
-          if (existeNodo(nodos, l[i][1]))
+        if (existeNodo(nodos, l[i][1]))
         {
-          
-        }else{
-             cout << "Creando nodo " << l[i][1] << endl;
-            nodos.push_back(new Nodo(l[i][1]));
-
         }
-        
-        
+        else
+        {
+            cout << "Creando nodo " << l[i][1] << endl;
+            nodos.push_back(new Nodo(l[i][1]));
+        }
     }
-    
 
     this->nodosCreados = nodos;
-
 }
 
-
-
-void Grafo::listaArevision(vector<int> l,int indexList )
+void Grafo::listaArevision(vector<int> l, int indexList)
 {
-    Nodo* n1 = getNodoById(l[0]);
-    Nodo* n2 = getNodoById(l[1]);
+    Nodo *n1 = getNodoById(l[0]);
+    Nodo *n2 = getNodoById(l[1]);
     int peso = l[2];
-    int index= indexList;
+    int index = indexList;
     if (this->revisionLinea(n1, n2, peso))
     {
-        
-         this->indexRespuesta.push_back(indexList
-        );
 
-
+        this->indexRespuesta.push_back(indexList);
     }
-    
 }
 
 Nodo *Grafo::getNodoById(int id)
 {
-        for (auto* nodo : nodosCreados) {
-        if (nodo->getId() == id) {
+    for (auto *nodo : nodosCreados)
+    {
+        if (nodo->getId() == id)
+        {
             return nodo;
         }
     }
-    return nullptr; 
+    return nullptr;
 }
 
 bool Grafo::revisionLinea(Nodo *n, Nodo *nr, int peso)
@@ -101,23 +96,35 @@ bool Grafo::revisionLinea(Nodo *n, Nodo *nr, int peso)
 void Grafo::imprmirRespuesta(vector<vector<int>> listaPesos)
 {
     int peso = 0;
-    std::cout << "Orden de arbol " << std::endl;
+    std::cout << "Conexiones usadas: ";
     for (size_t i = 0; i < this->indexRespuesta.size(); i++)
     {
-        std::cout << listaPesos[i][0] << " " << listaPesos[i][1] << " " << listaPesos[i][2] << std::endl;
-        peso += listaPesos[i][2];
+        int index = this->indexRespuesta[i];
+        std::cout << "[" << listaPesos[index][0] << "," << listaPesos[index][1] << "," << listaPesos[index][2] << "]";
+        peso += listaPesos[index][2];
+
+        if (i < this->indexRespuesta.size() - 1)
+        {
+            std::cout << ", ";
+        }
     }
-    std::cout << "Peso total del arbol: " << peso << std::endl;
+    std::cout << ". Peso final: " << peso << std::endl;
 }
 
-bool Grafo::buscarNodoRecursivo(Nodo* actual, Nodo* padre, int idBuscado) {
-    if (!actual) return false;
-    if (actual->getId() == idBuscado) return true;
+bool Grafo::buscarNodoRecursivo(Nodo *actual, Nodo *padre, int idBuscado)
+{
+    if (!actual)
+        return false;
+    if (actual->getId() == idBuscado)
+        return true;
 
-    for (const auto& conexion : actual->getConexiones()) {
-        Nodo* vecino = conexion.nodoVecino;
-        if (vecino != padre) {
-            if (buscarNodoRecursivo(vecino, actual, idBuscado)) {
+    for (const auto &conexion : actual->getConexiones())
+    {
+        Nodo *vecino = conexion.nodoVecino;
+        if (vecino != padre)
+        {
+            if (buscarNodoRecursivo(vecino, actual, idBuscado))
+            {
                 return true;
             }
         }
--- main.cpp
+++ main.cpp
@@ -7,48 +7,54 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
 
-    Nodo * n1 = new Nodo(1);
-    Nodo * n2 = new Nodo(2);
-    Nodo * n3 = new Nodo(3);
+    Nodo *n1 = new Nodo(1);
+    Nodo *n2 = new Nodo(2);
+    Nodo *n3 = new Nodo(3);
 
-    vector<vector<int> > listaPesosOrdenada;
-     
+    vector<vector<int>> listaPesosOrdenada;
+
     // Leer desde el archivo lista.txt
-    ifstream infile("lista.txt");
+    ifstream infile("list.txt");
     string line;
-    while (getline(infile, line)) {
+    while (getline(infile, line))
+    {
         istringstream iss(line);
         vector<int> elemento(3);
-        iss >> elemento[0] >> elemento[1] >> elemento[2];
-        listaPesosOrdenada.push_back(elemento);
+        if (iss >> elemento[0] >> elemento[1] >> elemento[2])
+        {
+            listaPesosOrdenada.push_back(elemento);
+        }
+    }
+    infile.close();
+
+    if (listaPesosOrdenada.empty())
+    {
+        cout << "Error: No se encontraron conexiones válidas en el archivo" << endl;
+        return 1;
     }
 
     // Ordenar por el último elemento de cada subvector
     sort(listaPesosOrdenada.begin(), listaPesosOrdenada.end(),
-        [](const vector<int>& a, const vector<int>& b) {
-            return a[2] < b[2];
-        }
-    );
-    
+         [](const vector<int> &a, const vector<int> &b)
+         {
+             return a[2] < b[2];
+         });
 
-    
-    vector<Nodo*> nodos;
+    vector<Nodo *> nodos;
     Grafo g;
 
     g.listaANodos(listaPesosOrdenada);
     for (size_t i = 0; i < listaPesosOrdenada.size(); i++)
     {
-         g.listaArevision(listaPesosOrdenada[i], i);
+        g.listaArevision(listaPesosOrdenada[i], i);
     }
-    
-   
-    //nodos[0]
-    g.imprmirRespuesta(listaPesosOrdenada);        
-    //g.revisionLinea(nodos[0], nodos[1], 0);
 
-    
+    // nodos[0]
+    g.imprmirRespuesta(listaPesosOrdenada);
+    // g.revisionLinea(nodos[0], nodos[1], 0);
 
     return 0;
 }
